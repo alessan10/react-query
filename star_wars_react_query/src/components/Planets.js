c1fafb95b863d7bace94b8e14492e1ee6b2a041e
alessandromauro.m@outlook.com <alex.mauro@live.it>
2021-03-16 12:51:46 +0100

    [Planet.js] - useQuery issues

---
+++ star_wars_react_query/src/components/Planets.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { QueryClientProvider, QueryClient, useQuery } from 'react-query'
+
+const fetchPlanets = async () => {
+    const res = await fetch('http://swapi.dev/api/planets/');
+    return res.json();
+}
+
+const Planets = () => {
+    const {data, status} = useQuery('planets', fetchPlanets)
+    console.log(data);
+    return (
+        <div>
+            <h2>Planets</h2>
+
+            {status === 'loading' && (
+                <div>Loading data</div>
+            )}
+
+            {status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === 'success' && (
+                <div>
+                    {data.results.map(planet => <div>{planet.name}</div>)}
+                </div>
+            )}            
+        </div>
+    );
+}
+
+export default Planets;
