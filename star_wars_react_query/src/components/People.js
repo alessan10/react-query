50fef6c397b6964d1ce8d80edd2c1839f3ed50b3
alessandromauro.m@outlook.com <alex.mauro@live.it>
2021-03-17 10:14:19 +0100

    app works

--- star_wars_react_query/src/components/People.js
+++ star_wars_react_query/src/components/People.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { QueryClientProvider, QueryClient, useQuery } from 'react-query'
+import Person from '../models/Person'
+
+const queryClient = new QueryClient();
+
+const fetchPeople = async () => {
+    const res = await fetch('http://swapi.dev/api/people/');
+    return res.json();
+}
 
 const People = () => {
+    const {data, status} = useQuery('people', fetchPeople);
+    console.log(data);
     return (
         <div>
             <h2>People</h2>
+
+            {status === 'loading' && (
+                <div>Loading data</div>
+            )}
+
+            {status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === 'success' && (
+                <div>
+                    {data.results.map(person => <Person key = {person.name} person={person} />)}
+                </div>
+            )}            
         </div>
     );
 }
 
-export default People;
+//high order function
+export default function Wrapped(){
+    return (<QueryClientProvider client = {queryClient}>
+        <People />
+        </QueryClientProvider>
+    );
+}
